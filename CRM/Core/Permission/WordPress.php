dd9fcc9f55c66f0414096b2bcb994e11b6bbbadc
Eileen McNaughton <eileen@fuzion.co.nz>
2014-07-22 14:29:44 +1200

    CRM-12499 for Word press

--- CRM/Core/Permission/WordPress.php
+++ CRM/Core/Permission/WordPress.php
@@ -89,6 +89,16 @@ class CRM_Core_Permission_WordPress {
    */
   static
   function check($str) {
+    $str = parent::translatePermission($str, 'WordPress', array(
+      'view user account' => 'administrator',
+    ));
+    if ($str == CRM_Core_Permission::ALWAYS_DENY_PERMISSION) {
+      return FALSE;
+    }
+    if ($str == CRM_Core_Permission::ALWAYS_ALLOW_PERMISSION) {
+      return TRUE;
+    }
+
     // for administrators give them all permissions
     if (!function_exists('current_user_can')) {
       return TRUE;
